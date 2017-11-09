80bad0f2d2c359f7e9c13966d83f4fb9230d9c19
LizbethFP <32282183+LizbethFP@users.noreply.github.com>
2017-11-08 22:08:03 -0500

    Update app.js
    Agregando intento de tres en línea

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
-window.onload = function() {
-  var board = document.querySelector('.board-js');
+//Declaración de objetos y variables
+var turn = 1;
+var whichTurn;
+var arrayBoard = new Array(9);
+var cells = document.getElementsByClassName("ticTacToe");
 
-  board.addEventListener('click', addX);
-};
 
-var centinel = true;
 
-function addX(event) {
-  if (centinel) {
-  event.target.textContent = 'X';
-  centinel = false;
-  } else {
-  event.target.textContent = 'O';
-  centinel = true;
-  }
+//Declaración de funciones
+function winner() {
+
+}
+
+function ticTacToe() {
+  alert("Funciona tic tac toe!")
+}
+
+function loadDocuent() {
+  document.getElementsByClassName("ticTacToe")[4].addEventListener("click", ticTacToe);
 }
 
 
 
+//Asignación de eventos
+window.addEventListener("load", loadDocuent);
 
+
+
+
+
+// window.onload = function() {
+//   var board = document.querySelector('.board-js');
+//
+//   board.addEventListener('click', addX);
+// };
+//
+// var centinel = true;
+//
+// function addX(event) {
+//   if (centinel) {
+//   event.target.textContent = 'X';
+//   centinel = false;
+//   } else {
+//   event.target.textContent = 'O';
+//   centinel = true;
+//   }
+// }
+//
 
 //   function(event) {
 //     // event.target.style.backgroundColor='yellow';
